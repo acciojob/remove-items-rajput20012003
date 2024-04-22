8268844364661446f4329fb55652ca897306ec6f
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-04-22 21:24:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Function to remove the selected color from the dropdown list
+function removeColor() {
+    // Get the select element
+    var selectElement = document.getElementById('colorSelect');
+    // Get the index of the selected option
+    var selectedIndex = selectElement.selectedIndex;
+    // Remove the selected option
+    selectElement.remove(selectedIndex);
+}
+
+// Add event listener to the button to trigger the removal of the selected color
+document.querySelector('input[type="button"]').addEventListener('click', removeColor);
+
